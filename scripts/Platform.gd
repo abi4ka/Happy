251dbffc2607a2f091ad5b017ba1cd0690a4683f
Nikita Tkalich <nikita161787@gmail.com>
2025-11-11 09:45:41 +0100

    moving platform

--- scripts/Platform.gd
+++ scripts/Platform.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
-var start_y
 @export var move_distance := 64.0
 @export var move_speed := 3.0
-var target_y
+@export var horizontal_move := true
+
+var start_pos: Vector2
+var target_pos: Vector2
 
 func _ready():
-	start_y = position.y
-	target_y = start_y
+	start_pos = position
+	target_pos = start_pos
 
 func lower():
-	target_y = start_y + move_distance
+	if horizontal_move:
+		target_pos.x = start_pos.x + move_distance
+	else:
+		target_pos.y = start_pos.y + move_distance
 
 func raise():
-	target_y = start_y
+	target_pos = start_pos
 
 func _process(delta):
-	position.y = lerp(position.y, target_y, delta * move_speed)
+	position = position.lerp(target_pos, delta * move_speed)
