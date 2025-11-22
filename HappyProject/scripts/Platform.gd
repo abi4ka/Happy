868e9847bf567955dfcdc6c9660cbc58caec7fcf
abik <nikita161787@gmail.com>
2025-11-22 14:08:40 +0100

    level3/moving platforms

--- HappyProject/scripts/Platform.gd
+++ HappyProject/scripts/Platform.gd
@@ -3,8 +3,10 @@ extends Node2D
 @export var move_distance := 64.0
 @export var move_speed := 3.0
 @export var horizontal_move := true
+@export var auto_move := false
 
 var start_pos: Vector2
+var end_pos: Vector2
 var target_pos: Vector2
 var direction: Vector2 = Vector2.ZERO
 
@@ -12,35 +14,59 @@ var hold_count: int = 0
 
 func _ready():
 	start_pos = position
+
+	if horizontal_move:
+		end_pos = start_pos + Vector2(move_distance, 0)
+	else:
+		end_pos = start_pos + Vector2(0, move_distance)
+
 	target_pos = start_pos
 
+
 func button_pressed():
+	if auto_move:
+		return
 	hold_count += 1
 	update_target()
 
+
 func button_released():
+	if auto_move:
+		return
 	hold_count = max(hold_count - 1, 0)
 	update_target()
 
+
 func update_target():
 	if hold_count > 0:
-		lower()
+		target_pos = end_pos
 	else:
-		raise()
+		target_pos = start_pos
 
-func lower():
-	if horizontal_move:
-		target_pos = start_pos + Vector2(move_distance, 0)
-	else:
-		target_pos = start_pos + Vector2(0, move_distance)
 	direction = (target_pos - position).normalized()
 
-func raise():
-	target_pos = start_pos
-	direction = (target_pos - position).normalized()
 
 func _process(delta):
+	if auto_move:
+		_auto_move(delta)
+	else:
+		_manual_move(delta)
+
+
+func _manual_move(delta):
 	if position.distance_to(target_pos) > move_speed * delta:
 		position += direction * move_speed * delta
 	else:
 		position = target_pos
+
+
+func _auto_move(delta):
+	var dir_to_target = (target_pos - position).normalized()
+
+	if position.distance_to(target_pos) > move_speed * delta:
+		position += dir_to_target * move_speed * delta
+	else:
+		if target_pos == start_pos:
+			target_pos = end_pos
+		else:
+			target_pos = start_pos
